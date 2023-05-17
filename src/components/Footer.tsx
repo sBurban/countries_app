462d0bcc416307bc91cfdda622c14f008a3ad13b
sBurban <sebburbano@gmail.com>
2023-05-17 07:11:54 -0500

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,18 @@ const Footer = () => {
         <footer className="footer">
             <div className="footer__wrapper">
 
+                <div className="copyright">
+                    <p>Copyright ©2020 30 Days Of React</p>
+                    <p>Join{' '}
+                        <a href="https://github.com/Asabeneh/30-Days-Of-React">
+                            30 Days of React challenge
+                        </a>
+                    </p>
+                    <small>
+                        Designed and Built by
+                        <a href="https://www.linkedin.com/in/asabeneh/">Asabeneh Yetayeh</a>
+                    </small>
+                </div>
 
                 <div className="credits">
                     <p>Recreated functionality on React Hooks + Typescript</p>
