af99270db3dbca20772c48a2706292b7c52a1635
sburban <sebburbano@gmail.com>
2024-01-24 23:22:21 -0500

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/countries_app/',
+  base: './',
+  // base: '/countries_app/',
   plugins: [react()],
 })
